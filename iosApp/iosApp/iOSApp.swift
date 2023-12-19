dbc5ef97234241beeb7947583a242e0d815e36fe
Alisher <alisherqazaqbaev@gmail.com>
2023-12-19 21:55:26 +0500

    add iosAPP

---
+++ iosApp/iosApp/iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosAppApp.swift
+//  iosApp
+//
+//  Created by Alisher on 19/12/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
