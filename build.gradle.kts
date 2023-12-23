2241597f344dd4ab0c9de3e0a78b1af8fa52bf8a
Alisher <alisherqazaqbaev@gmail.com>
2023-12-23 18:19:38 +0500

    add google services

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     alias(libs.plugins.kotlin.cocoapods) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.google.services) apply false
 }
 
 tasks.register("clean").configure {
